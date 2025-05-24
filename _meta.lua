fc6afee9619c4e02cec680930d68df5981e09939
pengcw <pydove@foxmail.com>
2025-05-24 15:24:14 +0800

    xxx

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "zlibrary",
     fullname = T("Z-library"),
     description = T("[[A plugin to search and download books from Z-library.]]"),
-    version = "1.1.9",
+    version = "1.1.11",
 }
